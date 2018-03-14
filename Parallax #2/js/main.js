f4a54c69fdea73a0d42c461c1f2b4393758eed63
edFortune <fortune.edmael1@gmail.com>
2018-03-14 15:30:07 -0400

    Add body containt in parallax #2

--- Parallax #2/js/main.js
+++ Parallax #2/js/main.js
@@ -1,4 +1,25 @@
 (function () {
 
+    init();
+
+    function init() {
+        $navBar = $('#nav-bar');
+        $navTop = $navBar.offset().top;
+
+        $(window).scroll(function (event) {
+            $scrollTop = $(this).scrollTop();
+
+            if ($scrollTop > $navTop) {
+                $navBar.addClass("sticky-nav-bar");
+                $navBar.css("opacity", 1);
+                $('#subtitle').removeClass("hidden");
+            } else {
+                $navBar.removeClass("sticky-nav-bar");
+                $navBar.css("opacity", 0);
+                $('#subtitle').addClass("hidden");
+            }
+        });
+
+    }
 
 })();
